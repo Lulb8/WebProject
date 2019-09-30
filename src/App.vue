356c392957bc9a746c9f73f9a3a545f8e3112a4b
lulb8 <bonneville@et.esiea.fr>
2019-09-30 21:25:27 +0200

    New navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
+    <nav-bar></nav-bar>
     <v-content>
       <HelloWorld/>
     </v-content>
@@ -22,15 +8,15 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    NavBar
   },
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app left>
+      <v-list dense>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-account-edit</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Register</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-account-lock</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-account-remove</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-heart-multiple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Favorites</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#C62828" dark>
+      <div class="flex-grow-0"></div>
+      <v-toolbar-title>Meals App</v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-footer color="#8e0000" app>
+      <div class="flex-grow-1"></div>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
