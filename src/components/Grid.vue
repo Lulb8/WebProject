cf41249bc8f3e5f7eba589238a8cdd689a7ef4ff
lulb8 <bonneville@et.esiea.fr>
2019-10-12 00:03:41 +0200

    Filter OK

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,68 +1,19 @@
 <template>
-  <section class="section">
-    <v-container fluid>
-      <v-combobox
-        v-model="model"
-        :filter="filter"
-        :hide-no-data="!search"
-        :items="items"
-        :search-input.sync="search"
-        hide-selected
-        label="Choose a category"
-        multiple
-        small-chips
-        solo
-      >
-        <template v-slot:no-data>
-          <v-list-item>
-            <span class="subheading">Create</span>
-            <v-chip :color="`${colors[nonce - 1]} lighten-3`" label small>{{ search }}</v-chip>
-          </v-list-item>
-        </template>
-
-        <template v-slot:selection="{ attrs, item, parent, selected }">
-          <v-chip
-            v-if="item === Object(item)"
-            v-bind="attrs"
-            :color="`${item.color} lighten-3`"
-            :input-value="selected"
-            label
-            small
-          >
-            <v-icon>{{ item.icon }}</v-icon>
-            <span class="pr-2">{{ item.text }}</span>
-            <v-icon small @click="parent.selectItem(item)">mdi-close-circle-outline</v-icon>
-          </v-chip>
-        </template>
-
-        <template v-slot:item="{ index, item }">
-          <v-text-field
-            v-if="editing === item"
-            v-model="editing.text"
-            autofocus
-            flat
-            background-color="transparent"
-            hide-details
-            solo
-          ></v-text-field>
-          <v-chip v-else :color="`${item.color} lighten-3`" dark label small>
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>{{ item.text }}</v-list-item-content>
-          </v-chip>
-          <div class="flex-grow-1"></div>
-        <v-list-item-action @click.stop>
-          <v-btn
-            icon
-            @click.stop.prevent="edit(index, item)"
-          >
-            <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
-          </v-btn>
-        </v-list-item-action>
-        </template>
-      </v-combobox>
-    </v-container>
+  <v-container fluid>
+  
+        <v-select
+          v-model="search"
+          :items="items"
+          label="Select a category"
+          outlined
+          small-chips
+          clearable
+          dense
+          :hint="`${ search }`"
+          persistent-hint
+          :hide-no-data="!search"
+          :search-input.sync="search"
+        ></v-select>
 
     <div class="container">
       <magic-grid>
@@ -75,7 +26,7 @@
         />
       </magic-grid>
     </div>
-  </section>
+  </v-container>
 </template>
 
 <script>
@@ -89,9 +40,8 @@ export default {
   },
   data () {
     return {
+      search: '',
       recipes: [],
-      activator: null,
-      attach: null,
       colors: [
         'brown',
         'red',
@@ -108,8 +58,6 @@ export default {
         'cyan',
         'deep-purple'
       ],
-      editing: null,
-      index: -1,
       items: [
         { header: 'Select a category' },
         {
@@ -190,11 +138,6 @@ export default {
           icon: 'mdi-filter-remove'
         }
       ], */
-      nonce: 1,
-      menu: false,
-      x: 0,
-      search: null,
-      y: 0
     }
   },
   mounted () {
@@ -204,54 +147,11 @@ export default {
         this.recipes = json.slice(0, 209)
       })
   },
-  /*
   computed: {
     filteredRecipes: function () {
       return this.recipes.filter(recipe => {
         return recipe.strCategory.toLowerCase().match(this.search.toLowerCase())
       })
-    }
-  },
-*/
-
-  watch: {
-    model (val, prev) {
-      if (val.length === prev.length) return
-      this.model = val.map(v => {
-        if (typeof v === 'string') {
-          v = {
-            text: v,
-            color: this.colors[this.nonce - 1]
-          }
-          this.items.push(v)
-          this.nonce++
-        }
-        return v
-      })
-    }
-  },
-
-  methods: {
-    edit (index, item) {
-      if (!this.editing) {
-        this.editing = item
-        this.index = index
-      } else {
-        this.editing = null
-        this.index = -1
-      }
-    },
-    filter (item, queryText, itemText) {
-      if (item.header) return false
-
-      const hasValue = val => val != null ? val : ''
-
-      const text = hasValue(itemText)
-      const query = hasValue(queryText)
-
-      return text.toString()
-        .toLowerCase()
-        .indexOf(query.toString().toLowerCase()) > -1
     }
   }
 }
